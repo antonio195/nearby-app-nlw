6cc05da30352967608454a91036fac4087b08064
antonio195 <antonio195br@gmail.com>
2024-12-14 00:35:28 -0300

    Add new some components

--- app/src/main/java/com/antoniocostadossantos/nearby/ui/components/button/NearbyButton.kt
+++ app/src/main/java/com/antoniocostadossantos/nearby/ui/components/button/NearbyButton.kt
@@ -1,4 +1,4 @@
-package com.antoniocostadossantos.nearby.ui.components
+package com.antoniocostadossantos.nearby.ui.components.button
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.Arrangement
---
+++ app/src/main/java/com/antoniocostadossantos/nearby/ui/components/category/NearbyCategoryFilterChip.kt
@@ -0,0 +1,69 @@
+package com.antoniocostadossantos.nearby.ui.components.category
+
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.antoniocostadossantos.nearby.data.model.NearbyCategory
+import com.antoniocostadossantos.nearby.ui.theme.Gray300
+import com.antoniocostadossantos.nearby.ui.theme.Gray400
+import com.antoniocostadossantos.nearby.ui.theme.GreenBase
+import com.antoniocostadossantos.nearby.ui.theme.Typography
+
+@Composable
+fun NearbyCategoryFilterChip(
+    modifier: Modifier = Modifier,
+    category: NearbyCategory,
+    isSelected: Boolean,
+    onClick: (isSelected: Boolean) -> Unit,
+) {
+    FilterChip(
+        modifier = modifier
+            .padding(2.dp)
+            .heightIn(min = 36.dp),
+        elevation = FilterChipDefaults.filterChipElevation(
+            elevation = 8.dp
+        ),
+        leadingIcon = {
+            category.icon?.let {
+                Icon(
+                    modifier = Modifier.size(16.dp),
+                    painter = painterResource(it),
+                    tint = if (isSelected) Color.White else Gray400,
+                    contentDescription = "Ícone de filtro"
+                )
+            }
+        },
+
+        border = FilterChipDefaults.filterChipBorder(
+            enabled = false,
+            selected = isSelected,
+            disabledBorderColor = Gray300,
+            borderWidth = 1.dp,
+            selectedBorderWidth = 0.dp,
+            selectedBorderColor = Color.Transparent
+        ),
+        colors = FilterChipDefaults.filterChipColors(
+            containerColor = Color.White,
+            selectedLabelColor = GreenBase
+        ),
+        selected = isSelected,
+        onClick = { onClick(isSelected) },
+        label = {
+            Text(
+                text = category.name,
+                style = Typography.bodyMedium,
+                color = if (isSelected) Color.White else Gray400
+            )
+        }
+    )
+
+}
